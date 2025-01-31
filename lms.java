fff322294c991d2107f50b58380b6c28b8f63586
sridathB8 <mantripragadasridath@gmail.com>
2025-01-31 20:34:46 +0530

    First commit

---
+++ lms.java
@@ -0,0 +1,90 @@
+public class lms{
+int bookid;
+
+String title;
+String author;
+
+void displayBookInfo()
+{
+	System.out.println("this will give info abt book");
+
+}
+
+void borrowBook()
+{
+	System.out.println("this function for helping to borror books");
+}
+
+void returnBook()
+{
+
+
+
+	System.out.println("iis for when the book is returned");
+}
+
+	public static void main(String args[]) 
+	{
+		System.out.println("This is for library");
+		lms l1 = new lms(); 
+		l1.title="Stepeh King";
+		System.out.println(l1.author);
+	}
+}
+
+
+public class Library
+{
+	System.out.println("This for library functions");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
